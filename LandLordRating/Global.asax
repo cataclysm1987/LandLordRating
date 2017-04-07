eb15dcf6e225b60dba4de8ba340c21168e7ffdec
cataclysm1987 <conklin.eric@gmail.com>
2017-04-07 16:32:57 -0400

    Add project files.

---
+++ LandLordRating/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LandLordRating.MvcApplication" Language="C#" %>
---
+++ LandLordRating/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LandLordRating.Startup))]
+namespace LandLordRating
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
