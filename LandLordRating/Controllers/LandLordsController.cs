dea055df980af03168b18de7173cabca5e717a6b
cataclysm1987 <conklin.eric@gmail.com>
2017-04-07 19:04:25 -0400

    Add landlord controller and views

---
+++ LandLordRating/Controllers/LandLordsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LandLordRating.Models;
+
+namespace LandLordRating.Controllers
+{
+    public class LandLordsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: LandLords
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.LandLords.ToListAsync());
+        }
+
+        // GET: LandLords/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LandLord landLord = await db.LandLords.FindAsync(id);
+            if (landLord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(landLord);
+        }
+
+        // GET: LandLords/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LandLords/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "LandLordId,FullName,PhoneNumber,City,State")] LandLord landLord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.LandLords.Add(landLord);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(landLord);
+        }
+
+        // GET: LandLords/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LandLord landLord = await db.LandLords.FindAsync(id);
+            if (landLord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(landLord);
+        }
+
+        // POST: LandLords/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "LandLordId,FullName,PhoneNumber,City,State")] LandLord landLord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(landLord).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(landLord);
+        }
+
+        // GET: LandLords/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LandLord landLord = await db.LandLords.FindAsync(id);
+            if (landLord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(landLord);
+        }
+
+        // POST: LandLords/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            LandLord landLord = await db.LandLords.FindAsync(id);
+            db.LandLords.Remove(landLord);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
