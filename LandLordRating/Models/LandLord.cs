10730a9b2121dc5d124670800818bd999c7da760
merfie <sean.merfie@gmail.com>
2017-04-08 00:25:35 -0400

    Adding pretty bootstrap css to /LandLords/Index, /LandLords/Create, and /LandLords/CreateRating

--- LandLordRating/Models/LandLord.cs
+++ LandLordRating/Models/LandLord.cs
@@ -10,7 +10,9 @@ namespace LandLordRating.Models
     {
         [Key]
         public int LandLordId { get; set; }
+        [Display(Name = "Name")]
         public string FullName { get; set; }
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         public string City { get; set; }
         public string State { get; set; }
